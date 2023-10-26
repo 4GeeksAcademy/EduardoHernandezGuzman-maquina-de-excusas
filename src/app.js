114e37ac9412590d53dbc6ebec875e51b3819bdf
EduardoHG <139759297+EduardoHernandezGuzman@users.noreply.github.com>
2023-10-26 18:43:57 +0000

    Se añadieron nuevas excusas al app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,15 @@ window.onload = function() {
     "Un ser venido de otro planeta",
     "Godzilla",
     "Drácula",
-    "Cthulhu"
+    "Cthulhu",
+    "El payaso de los globos",
+    "El dios primigenio",
+    "La bruja malvada",
+    "El zombi hambriento",
+    "El fantasma vengativo",
+    "El demonio infernal",
+    "El espíritu maligno",
+    "El monstruo marino"
   ];
   let action = [
     "comió",
@@ -18,7 +26,11 @@ window.onload = function() {
     "desintegró",
     "pulverizó",
     "escondió",
-    "partió"
+    "partió",
+    "aplastó",
+    "aniquiló",
+    "incineró",
+    "trituró"
   ];
   let what = [
     "las llaves de mi casa",
@@ -26,7 +38,16 @@ window.onload = function() {
     "mi coche nuevo",
     "las gafas de mi abuela",
     "los apuntes de HTML",
-    "el monopatín de mi hermano pequeño"
+    "el monopatín de mi hermano pequeño",
+    "el teléfono de mi madre",
+    "el lápiz de color rojo",
+    "el juguete favorito de mi hijo",
+    "el jersey de lana",
+    "la taza de café",
+    "la mochila del colegio",
+    "el ordenador portátil",
+    "la cartera de cuero",
+    "el anillo de oro"
   ];
   let when = [
     "después del almuerzo",
@@ -35,7 +56,20 @@ window.onload = function() {
     "durante el viaje de fin de curso",
     "depués de las clases",
     "durante la misa del domingo",
-    "durante la proyección de la película"
+    "durante la proyección de la película",
+    "después del partido",
+    "durante las vacaciones",
+    "después del trabajo",
+    "durante la reunión",
+    "durante la cena",
+    "antes de la boda",
+    "durante la tormenta",
+    "después de la ducha",
+    "durante la conferencia",
+    "durante el partido de fútbol",
+    "después del partido de tenis",
+    "durante la cena de Navidad",
+    "después del Año Nuevo"
   ];
 
   function createExcuse() {
